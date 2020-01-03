7b94770a21ddef1b37fdbcc86b6d9fc8a3164330
Administrator <Administrator@I91T1FM30VWNVX5>
2020-01-03 10:36:07 +0800

    2020/1/3/测试

---
+++ _test/src/_test/Test1.java
@@ -0,0 +1,12 @@
+package _test;
+/**
+* @author 作者：caojunchao
+* @version v.1.0 创建时间：上午10:29:51
+* @description 描述：
+*/
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("main");
+	}
+}
